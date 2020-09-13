bc7036193ef87f71ed7ee3acb498e7dad3671589
Kishore <65759433+kishorgollapalli@users.noreply.github.com>
2020-09-13 20:19:52 +1000

    Few more comments

--- ServicePathAudit.ps1
+++ ServicePathAudit.ps1
@@ -1,5 +1,6 @@
 # Adding some comments about the script 
-
+# Script goes to every services, If service path have any spaces and the path is not included in Quotes, 
+# the script will write the service name and path to output file 
 
 $ResultsLogFile = "C:\Temp\results.log"
 $HostName = $env:COMPUTERNAME
